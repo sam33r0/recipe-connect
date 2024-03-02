55641db6ef0655071454d8364c84de074314c592
sam33r0 <srivastavasameer2002@gmail.com>
2024-03-02 16:23:59 +0530

    recipe delete and update visibility

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
-function About() {
+export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-16 bg-white">
+      <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+        <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+          <div className="md:5/12 lg:w-5/12">
+            <img
+              src="https://res.cloudinary.com/de9rb613m/image/upload/v1709369488/bg-image-about-recipe.webp"
+              alt="image"
+            />
+          </div>
+          <div className="md:7/12 lg:w-6/12">
+            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+              Cook, Connect, and Create: Welcome to Recipe Sharing!
+            </h2>
+            <p className="mt-6 text-gray-600">
+              Indulge your culinary curiosity at our recipe sharing hub! Here,
+              we celebrate the art of cooking and the joy of sharing delicious creations.
+              From mouthwatering meals to decadent desserts,
+              our platform is your go-to destination for inspiration, collaboration,
+              and culinary adventure.
+            </p>
+            <p className="mt-4 text-gray-600">
+              Join our community of food lovers, and let's explore the endless
+              possibilities of flavor together!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default About
--- src/components/pages/RecipePage.jsx
+++ src/components/pages/RecipePage.jsx
@@ -5,16 +5,35 @@ import { useParams } from 'react-router-dom';
 import { ScrollArea } from "./../../@/components/ui/scroll-area"
 import { useSelector } from 'react-redux'
 import { Button } from "../../@/components/ui/button"
+import { Link, useNavigate } from 'react-router-dom'
 import {
   Avatar,
   AvatarFallback,
   AvatarImage,
 } from '../../@/components/ui/avatar.jsx'
+import { Switch } from "../../@/components/ui/switch"
+
+
 function RecipePage() {
+  const navigate = useNavigate();
+  const deleteRecipe = async () => {
+    const delet=await axios.post((backendUri + "/recipe/delete-recipe"), {
+      recId: param.id,
+    },
+    {
+      headers: {
+        'Authorization': `Bearer ${userdata?.refreshToken}`
+      },
+      withCredentials: true
+    })
+    console.log(delet);
+    navigate('/')
+  }
+  const [visibility, setVisibility] = useState(true);
   const authStatus = useSelector((state) => state.auth.status);
+  const userdata = useSelector((state) => state.auth.userData);
   const param = useParams();
   const [recipe, setRecipe] = useState({});
-  console.log(param.id);
   useEffect(() => {
     (async () => {
       const reci = await axios.post((backendUri + "/recipe/get-recipe"), {
@@ -22,9 +41,23 @@ function RecipePage() {
       },
         {})
       setRecipe(reci.data.data);
-      console.log(reci.data.data);
+      setVisibility(reci.data.data.recipe.visibility);
     })()
   }, [])
+  useEffect(() => {
+    (async () => {
+      await axios.post((backendUri + "/recipe/update-visibility"), {
+        id: param.id,
+        visibility: visibility
+      },
+        {
+          headers: {
+            'Authorization': `Bearer ${userdata?.refreshToken}`
+          },
+          withCredentials: true
+        })
+    })()
+  }, [visibility])
   return (
     <>
       <div className='flex w-full'>
@@ -45,7 +78,7 @@ function RecipePage() {
           </div>
         </div>
         <div className='bg-green-100 h-[540px] w-full'>
-          <div className={`${recipe?.recipe?.visibility != true ? "bg-gray-200 text-gray-500" : recipe?.recipe?.category == "VEG" ? "bg-lime-300 text-red-600" : recipe?.recipe?.category == "NONVEG" ? "bg-red-600 text-white" : "bg-yellow-400"} p-2 text-xl font-bold text-center`}>
+          <div className={`${visibility != true ? "bg-gray-200 text-gray-500" : recipe?.recipe?.category == "VEG" ? "bg-lime-300 text-red-600" : recipe?.recipe?.category == "NONVEG" ? "bg-red-600 text-white" : "bg-yellow-400"} p-2 text-xl font-bold text-center`}>
             {recipe?.recipe?.name}
           </div>
           <ScrollArea className="h-[320px] w-full p-3 bg-gray-300 rounded-md">
@@ -57,24 +90,41 @@ function RecipePage() {
             <span>Cooking Time: &nbsp; </span>
             <span className={`${recipe?.recipe?.cookingTime.includes("1 hour") ? " text-yellow-500" : recipe?.recipe?.cookingTime.includes("2 hour") || recipe?.recipe?.cookingTime.includes("3 hour") ? " text-red-500 " : " text-green-500 "}`}>{recipe?.recipe?.cookingTime}</span>
           </div>
-          <div>
-            <div className='flex flex-col justify-end px-4 '>
-              <div>Author:-</div>
-              
-              <Avatar>
-                <AvatarImage src={recipe?.user?.avatar} alt="avatar" />
-                <AvatarFallback>NA</AvatarFallback>
-              </Avatar>
+          {!authStatus && (
+            <div>
+              <div className='flex flex-col justify-end px-4 '>
+                <div>Author:-</div>
 
-              {recipe?.user?.fullName}
+                <Avatar>
+                  <AvatarImage src={recipe?.user?.avatar} alt="avatar" />
+                  <AvatarFallback>NA</AvatarFallback>
+                </Avatar>
+
+                {recipe?.user?.fullName}
+              </div>
+            </div>
+          )}
+
+          {authStatus && (<div className='flex justify-between mx-8 mt-8'>
+            <div>
+              <Switch
+                checked={visibility}
+                onCheckedChange={() => setVisibility(!visibility)}
+                id="visi"
+              />
+              <label className="mx-2" htmlFor="visi">Visibility</label>
+            </div>
+            <div>
+              <Button onClick={deleteRecipe}>
+                Delete &nbsp;
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                  <path fill="#E74C3C" d="M19 6h-3V4.5A2.5 2.5 0 0 0 13.5 2h-3A2.5 2.5 0 0 0 8 4.5V6H5a1 1 0 0 0 0 2h1v12a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V8h1a1 1 0 0 0 0-2zm-9-.5A1.5 1.5 0 0 1 10.5 4h3a1.5 1.5 0 0 1 1.5 1.5V6h-6V4.5zM17 20H7a2 2 0 0 1-2-2V8h12v10a2 2 0 0 1-2 2z" />
+                </svg>
+              </Button>
+
             </div>
           </div>
-          <div className='flex justify-center'>
-            {authStatus && (
-              <Button className="mt-2" onClick={console.log("hello")}>
-                Edit Recipe
-              </Button>)}
-          </div>
+          )}
         </div>
 
       </div>
