7f455f0d6529f1f5a151c5c73ab1096b0c21310d
sam33r0 <srivastavasameer2002@gmail.com>
2024-03-14 16:08:09 +0530

    logout changes

--- src/components/Header/YourProfile.jsx
+++ src/components/Header/YourProfile.jsx
@@ -22,45 +22,47 @@ import { backendUri } from './../../envconfig.js'
 function YourProfile() {
     const user = useSelector((state) => state.auth.userData);
     const avatar = user.avatar;
-    const navigate= useNavigate();
+    const navigate = useNavigate();
     const dispath = useDispatch();
+    const accessToken = useSelector((state) => state.auth.accessToken);
     const logoutHandler = () => {
         axios.post((backendUri + '/users/logout'), {}, {
             headers: {
                 'Content-Type': 'multipart/form-data',
-                'Authorization': 'Bearer <token>'
+                'Authorization': `Bearer ${accessToken}`
             },
             withCredentials: true
         }).then((res) => {
-            //console.log(res);
+            console.log(res);
             dispath(logout());
+            navigate('/about');
         })
     }
     const myRecipe = () => {
         navigate('/my-recipe')
     }
-    const changePass= ()=>{
+    const changePass = () => {
         navigate('/user/change-password')
     }
-    const updateAvata= ()=>{
+    const updateAvata = () => {
         navigate('/user/update-avatar')
     }
-    const updateAcco=()=>{
+    const updateAcco = () => {
         navigate('/user/update-account')
     }
     return (
         <div className='flex'>
             <DropdownMenu className='text-gray-800 hover:bg-gray-50 hover:text-orange-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none'>
-                <DropdownMenuTrigger>{user.fullName.charAt(0).toUpperCase()+ user.fullName.substring(1,user.fullName.indexOf(' '))}&nbsp;&nbsp;</DropdownMenuTrigger>
+                <DropdownMenuTrigger>{user.fullName.charAt(0).toUpperCase() + user.fullName.substring(1, user.fullName.indexOf(' '))}&nbsp;&nbsp;</DropdownMenuTrigger>
                 <DropdownMenuContent>
                     <DropdownMenuLabel>My Account</DropdownMenuLabel>
                     <DropdownMenuSeparator />
-                    
+
                     <DropdownMenuItem><button className="w-full h-full text-left" onClick={logoutHandler}>My Recipes</button></DropdownMenuItem>
                     <DropdownMenuItem><button className="w-full h-full text-left" onClick={changePass}>Change Password </button></DropdownMenuItem>
                     <DropdownMenuItem><button className="w-full h-full text-left" onClick={updateAvata}>Update Profile Photo</button></DropdownMenuItem>
                     <DropdownMenuItem><button className="w-full h-full text-left" onClick={updateAcco}>Update Name or email</button></DropdownMenuItem>
-                    <DropdownMenuItem><button className="w-full h-full text-left" onClick={logoutHandler}>Logout</button></DropdownMenuItem>    
+                    <DropdownMenuItem><button className="w-full h-full text-left" onClick={logoutHandler}>Logout</button></DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
 
